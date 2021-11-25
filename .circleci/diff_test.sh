4ab120997290a1801a97c4fdd243664f7aeaacf5
Lester Padul <padullester@gmail.com>
2021-11-25 15:32:00 +0800

    NC-1: add correct file

--- .circleci/diff_test.sh
+++ .circleci/diff_test.sh
@@ -1,3 +1,5 @@
-for i in $(git diff --name-only | grep ".php"); do
-    cd .. && php -d error_reporting=32767 -l $i
+for i in $(git diff origin/master..origin/$(git rev-parse --abbrev-ref HEAD) --name-only | grep ".php"); do
+    echo "Checking file -> $i"
+    message=$(php -d error_reporting=32767 -l $i 2>&1)
+    echo $message
 done
