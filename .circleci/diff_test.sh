93fb39ea1d481659ad9b348df89e1c834cc592f6
Lester Padul <padullester@gmail.com>
2021-11-25 15:52:59 +0800

    NC-1: add error

--- .circleci/diff_test.sh
+++ .circleci/diff_test.sh
@@ -1,5 +1,10 @@
 for i in $(git diff origin/master..origin/$(git rev-parse --abbrev-ref HEAD) --name-only | grep ".php"); do
     echo "Checking file -> $i"
-    message=$(php -d error_reporting=32767 -l $i 2>&1)
-    echo $message
+    OUTPUT=$(php -d error_reporting=32767 -l $i 2>&1)
+    CHECK="No syntax errors detected"
+    if !(grep -q "$CHECK" <<< "$OUTPUT"); then
+        echo "ERROR EXISTS!"
+        echo $OUTPUT
+        exit 1;
+    fi
 done
