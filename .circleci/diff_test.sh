2c04741015ab182e9fed9e230c63cfddd8728148
Lester Padul <padullester@gmail.com>
2021-11-25 16:05:04 +0800

    NC-1: add correct error messages

--- .circleci/diff_test.sh
+++ .circleci/diff_test.sh
@@ -1,9 +1,12 @@
 for i in $(git diff origin/master..origin/$(git rev-parse --abbrev-ref HEAD) --name-only | grep ".php"); do
+    # check file
     echo "Checking file -> $i"
+    
+    # try linting php file
     OUTPUT=$(php -d error_reporting=32767 -l $i 2>&1)
     CHECK="No syntax errors detected"
     
-    # check if h
+    # check if has error
     if !(grep -q "$CHECK" <<< "$OUTPUT"); then
         echo "ERROR EXISTS!"
         echo $OUTPUT
