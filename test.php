e9e24d9bc25c89f764ad3804b5f9b1c2f6880a0e
Lester Padul <padullester@gmail.com>
2021-11-26 16:54:52 +0800

    NC-1: add wrong code

--- test.php
+++ test.php
@@ -4,6 +4,69 @@ include "index.php";
 echo 1
 
 echo 0/1
+echo 0/0
 
+doThisMethod();
+
+
+echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
+
+doThisMethod();echo 1
+
+echo 0/1
 
 doThisMethod();
