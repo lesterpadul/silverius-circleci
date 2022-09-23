850090e476bf66bc78c547507e1fc203c4dacde7
Lester Padul <padullester@gmail.com>
2022-09-23 13:45:39 +0800

    deploy/stg: add actp

--- .github/shell/diff_test.sh
+++ .github/shell/diff_test.sh
@@ -13,3 +13,20 @@ for i in $(find "app" -type f | grep ".php"); do
         exit 1;
     fi
 done
+
+
+for i in $(find "app" -type f | grep ".ctp"); do
+    # check file
+    echo "Checking file -> $i"
+
+    # try linting php file
+    OUTPUT=$(php -d error_reporting=32767 -l $i 2>&1)
+    CHECK="No syntax errors detected"
+
+    # check if has error
+    if !(grep -q "$CHECK" <<< "$OUTPUT"); then
+        echo "ERROR EXISTS!"
+        echo $OUTPUT
+        exit 1;
+    fi
+done
