9fb0c63cf178ee0d11de58b8deabf1a38a116b37
Lester Padul <padullester@gmail.com>
2022-09-23 13:39:42 +0800

    deploy/stg: add  changes

--- .github/shell/diff_test.sh
+++ .github/shell/diff_test.sh
@@ -1,17 +1,15 @@
-ls -la
+for i in $(find "app/Config" -type f | grep ".php"); do
+    # check file
+    echo "Checking file -> $i"
 
-# for i in $(find /app/config -type f | grep ".php"); do
-#     # check file
-#     echo "Checking file -> $i"
+    # try linting php file
+    OUTPUT=$(php -d error_reporting=32767 -l $i 2>&1)
+    CHECK="No syntax errors detected"
 
-#     # try linting php file
-#     OUTPUT=$(php -d error_reporting=32767 -l $i 2>&1)
-#     CHECK="No syntax errors detected"
-
-#     # check if has error
-#     if !(grep -q "$CHECK" <<< "$OUTPUT"); then
-#         echo "ERROR EXISTS!"
-#         echo $OUTPUT
-#         exit 1;
-#     fi
-# done
+    # check if has error
+    if !(grep -q "$CHECK" <<< "$OUTPUT"); then
+        echo "ERROR EXISTS!"
+        echo $OUTPUT
+        exit 1;
+    fi
+done
